4e9f571f30ab74de8b2b61293cf1a915e31566ba
mikeyuku <mikeyuko@gmail.com>
2022-03-22 21:50:47 +0800

    Initial commit

---
+++ listview_learn/lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home: MyApp(),
+  ));
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Listview Learning'),
+        centerTitle: true,
+      ),
+    );
+  }
+}
